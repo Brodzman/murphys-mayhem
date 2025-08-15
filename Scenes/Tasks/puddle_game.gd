162f769d7c9c758d67278fe33db532844c3be601
Brodzman <thbrodz@gmail.com>
2025-08-15 12:33:37 +1000

    Revert "Set puddles on a unique scene"
    This reverts commit fe04385aac962e2e1604223f2f4752c2a5c34aae.

--- Scenes/Tasks/puddle_game.gd
+++ Scenes/Tasks/puddle_game.gd
@@ -1,12 +1,8 @@
 extends Control
 
-@onready var mop: Button = $mop
+@onready var cloth: Button = $cloth
 
 var mouse_held = false
-
-func _ready() -> void:
-	pass
-
 
 func _on_button_button_down() -> void:
 	mouse_held = true
@@ -17,10 +13,5 @@ func _on_cloth_button_up() -> void:
 		
 func _process(delta: float) -> void:
 	if mouse_held == true:
-		$mop.set_position(get_global_mouse_position())
-		
-		
-
-
-func _on_puddle_mouse_entered() -> void:
-	$Puddle.visible = false
+		cloth.set_position(get_global_mouse_position())
+	
