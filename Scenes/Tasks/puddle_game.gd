71ce72354131f58b0003d135e2b8afd331808a79
Brodzman <thbrodz@gmail.com>
2025-08-15 12:33:35 +1000

    Revert "Add modular spawning to mop task"
    This reverts commit 47d115521157785628ff1abf85b1a04668a494b8.

--- Scenes/Tasks/puddle_game.gd
+++ Scenes/Tasks/puddle_game.gd
@@ -2,28 +2,12 @@ extends Control
 
 @onready var mop: Button = $mop
 
-var puddle = load("res://Scenes/Tasks/puddle.tscn")
-var puddle_amount = 3
-
 signal mop_held(value)
 
 var mouse_held = false
 
 func _ready() -> void:
-	var spawn_points = [
-		$SpawnPoint1.position,
-		$SpawnPoint2.position,
-		$SpawnPoint3.position,
-		$SpawnPoint4.position,
-		$SpawnPoint5.position,
-		$SpawnPoint6.position,
-	]
-	for amount in puddle_amount:
-		var new_puddle = puddle.instantiate()
-		add_child(new_puddle)
-		spawn_points.shuffle()
-		new_puddle.position = spawn_points[0]
-		spawn_points.remove_at(0)
+	pass
 
 
 func _on_button_button_down() -> void:
